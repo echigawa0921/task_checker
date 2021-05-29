6f8c7f3e1f78d6d2704a39707056be3e53fe8408
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2021-05-29 21:15:47 +0900

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,3 @@ import React from "react";
 import DoneAll from "@material-ui/icons/DoneAll";
 import "./style.css";
 
-export const Header = () => {
-  return (
-    <div className="header">
-      <DoneAll className="header_icon" fontSize="large"></DoneAll>
-      <span className="header_title">Task Checker</span>
-    </div>
-  );
-};
