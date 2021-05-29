bfbbc104d732a7b743a3501d79f42fe6d8819413
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2021-05-29 21:16:04 +0900

    ヘッダー表示（確認）

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,3 +2,11 @@ import React from "react";
 import DoneAll from "@material-ui/icons/DoneAll";
 import "./style.css";
 
+export const Header = () => {
+  return (
+    <div className="header">
+      <DoneAll className="header_icon" fontSize="large"></DoneAll>
+      <span className="header_title">Task Checker</span>
+    </div>
+  );
+};
